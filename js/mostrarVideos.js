423c422a9764ee1cdb48332209a46d9c6bcb0772
Ayla Natalia Martins Ferreira <aylantalia08@hotmail.com>
2024-05-03 16:30:48 -0300

    finalizado

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -1,8 +1,8 @@
 import { conectaAPI } from "./conectaAPI.js";
 
-const lista = document.querySelector("[data-lista-]");
+const lista = document.querySelector("[data-lista]");
 
-function constroiCard(titulo, descricao, url, imagem) {
+export default function constroiCard(titulo, descricao, url, imagem) {
   const video = document.createElement("li");
   video.className = "videos__item";
   video.innerHTML = `
@@ -21,8 +21,12 @@ function constroiCard(titulo, descricao, url, imagem) {
 }
 
 async function listaVideo() {
+  try{
   const listaApi = await conectaAPI.listaVideos();
   listaApi.forEach((elemento) => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+  } catch{
+    lista.innerHTML = `<h2 class="mensagem_titulo">Não foi possível carregar a lista de vídeos</h2>`;
+  }
 }
 
 listaVideo();
